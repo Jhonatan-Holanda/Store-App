a6421b4abb176cbb7a58bba1f5bd04996feb1453
Jhonatan Holanda Maia <jhonatan_holanda@hotmail.com>
2023-08-27 18:38:25 -0300

    style: :lipstick: Create style guide of project

---
+++ src/theme/defaults.ts
@@ -0,0 +1,23 @@
+export default {
+  COLORS:{
+    WHITE_200:'#E5E5E5',
+    WHITE_100:'#EFEFEF',
+
+    RED_DARK: '#AA2834',
+
+    GRAY_400: '#1B2028',
+    GRAY_300: '#292526',
+    GRAY_200: '#A4AAAD',
+    GRAY_100: '#CDCDCD',
+  },
+  FONT_FAMILY: {
+    REGULAR: 'Roboto_400Regular',
+    BOLD: 'Roboto_700Bold'
+  },
+  FONT_SIZE: {
+    SM: 14,
+    MD: 16,
+    LG: 18,
+    XL: 24
+  }
+}
