41dee9448d437906974a4013989a33eef9f0d4ed
Jhonatan Holanda Maia <jhonatan_holanda@hotmail.com>
2023-08-28 01:18:06 -0300

    feat: :sparkles: Created mock of various products

--- src/screens/Products/styles.ts
+++ src/screens/Products/styles.ts
@@ -6,15 +6,3 @@ export const Container = styled(SafeAreaView)`
   background-color: ${({theme}) => theme.COLORS.WHITE_200};
   padding: 24px;
 `
-
-export const Title = styled.Text`
-  text-align: center;
-  margin: 10px 0px 30px;
-
-  ${({theme}) => css`
-    color: ${theme.COLORS.GRAY_400}
-    font-family: ${theme.FONT_FAMILY.BOLD};
-    font-size: ${theme.FONT_SIZE.XL}px;
-    font-weight: bold;
-  `}
-`
