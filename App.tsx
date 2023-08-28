ac6bbc14de022e1f2165e50cdd78ac5119d150aa
Jhonatan Holanda Maia <jhonatan_holanda@hotmail.com>
2023-08-28 01:06:58 -0300

    feat: :lipstick: Include new font monospace for name products

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { StatusBar, ActivityIndicator } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
+import { RobotoMono_600SemiBold, RobotoMono_700Bold } from '@expo-google-fonts/roboto-mono';
 import { useFonts, EncodeSans_400Regular, EncodeSans_700Bold } from '@expo-google-fonts/encode-sans';
 
 import defaultTheme from './src/theme/default';
+import { Products } from './src/screens/Products';
 import { DetailProduct } from './src/screens/DetailProduct';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({EncodeSans_400Regular, EncodeSans_700Bold})
+  const [fontsLoaded] = useFonts({RobotoMono_600SemiBold, RobotoMono_700Bold, EncodeSans_400Regular, EncodeSans_700Bold})
 
   return (
     <ThemeProvider theme={defaultTheme}>
