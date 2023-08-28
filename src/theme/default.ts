c7a0b684038bfd764f59b06158891fff4abab897
Jhonatan Holanda Maia <jhonatan_holanda@hotmail.com>
2023-08-28 01:19:16 -0300

    style: :lipstick: Adding new colors and fonts

--- src/theme/default.ts
+++ src/theme/default.ts
@@ -2,17 +2,21 @@ export default {
   COLORS:{
     WHITE_200:'#E5E5E5',
     WHITE_100:'#EFEFEF',
+    WHITE:'#FFFFFF',
 
     RED_DARK: '#AA2834',
 
-    GRAY_400: '#1B2028',
-    GRAY_300: '#292526',
+    GRAY_500: '#1B2028',
+    GRAY_400: '#292526',
+    GRAY_300: '#878787',
     GRAY_200: '#A4AAAD',
     GRAY_100: '#CDCDCD',
   },
   FONT_FAMILY: {
-    REGULAR: 'Roboto_400Regular',
-    BOLD: 'Roboto_700Bold'
+    REGULAR: 'EncodeSans_400Regular',
+    BOLD: 'EncodeSans_700Bold',
+    SEMIBOLD_MONO: 'RobotoMono_600SemiBold',
+    BOLD_MONO: 'RobotoMono_700Bold'
   },
   FONT_SIZE: {
     SM: 12,
