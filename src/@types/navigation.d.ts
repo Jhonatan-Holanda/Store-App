fc36b4dc7de4c5316e11aaf9c7646fa9b8540507
Jhonatan Holanda Maia <jhonatan_holanda@hotmail.com>
2023-08-29 15:25:29 -0300

    feat: :label: Created a type for navigation

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,11 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      products: undefined;
+      detail: {
+        id:number
+      };
+      cart:undefined
+    }
+  }
+}
