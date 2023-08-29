75f65c5c2f2c7dbe5d1869054b9d96d70f42f1c0
Jhonatan Holanda Maia <jhonatan_holanda@hotmail.com>
2023-08-29 17:13:50 -0300

    feat: :sparkles: Created count of quantity with usestate

--- src/components/QuantityCount/index.tsx
+++ src/components/QuantityCount/index.tsx
@@ -1,25 +1,36 @@
 import { Icon } from '../IconComponent'
 import { useTheme } from 'styled-components/native'
 import { ContainerQuantity, QuantityProduct, CountQuantity } from './styles'
+import { AppError } from '../../utils/AppError';
 
 type Props = {
-  sizeIcon: number
+  sizeIcon: number;
+  value: number;
+  setQuantity: (number:number) => void;
 }
 
-export function QuantityCount({sizeIcon}: Props) {
+export function QuantityCount({sizeIcon, value, setQuantity}: Props) {
   const theme = useTheme();
+
+  function handleDecreaseQuantity(){
+    if(value === 0) {
+      return
+    }else {
+      setQuantity( value - 1)
+    }
+  }
 
   return(
     <ContainerQuantity>
-      <CountQuantity>
+      <CountQuantity onPress={handleDecreaseQuantity}>
         <Icon iconName="minuscircleo" size={sizeIcon} color={theme.COLORS.GRAY_400}/>
       </CountQuantity>
         
       <QuantityProduct>
-        0
+        {value}
       </QuantityProduct>
 
-      <CountQuantity>
+      <CountQuantity onPress={() => setQuantity( value + 1)}>
         <Icon iconName="pluscircleo" size={sizeIcon} color={theme.COLORS.GRAY_400}/>
       </CountQuantity>
     </ContainerQuantity>
