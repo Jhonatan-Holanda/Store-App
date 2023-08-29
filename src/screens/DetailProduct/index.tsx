e4b50f41fcae94fcf5c12ccd56cebf022151688a
Jhonatan Holanda Maia <jhonatan_holanda@hotmail.com>
2023-08-29 17:33:08 -0300

    feat: :sparkles: Consuming and displaying the return from our api

--- src/screens/DetailProduct/index.tsx
+++ src/screens/DetailProduct/index.tsx
@@ -1,59 +1,80 @@
 import { useTheme } from "styled-components/native";
+import { useState, useEffect } from 'react';
+import { useNavigation, useRoute } from "@react-navigation/native";
 
 import { HighLight } from "../../components/HighLight"
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { QuantityCount } from "../../components/QuantityCount";
+import { Loading } from "../../components/Loading";
+
+import { ProductProps } from '../../@types/productsDTO';
 
 import { Container, Image, ViewImageCenter, Value, ContainerValue } from "./styles";
+import { getProductByID } from '../../services/products';
+import { ScrollView, View } from "react-native";
 
-export type CardProductProps = {
-  image: string;
-  title: string;
-  description: string;
-  value: string;
-} 
+type RouteParams = {
+  id: string
+}
 
 export function DetailProduct(){
+  const [product, setProduct] = useState<ProductProps>();
+  const [loading, setLoading] = useState(false);
+  const [quantity, setQuantity] = useState(0);
   const theme = useTheme();
 
-  const mockProduct: CardProductProps = {
-    title: 'Mens Casual Slim Fit',
-    image: 'https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg',
-    description: 'The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.',
-    value: '15.99',
-  };
+  const navigation = useNavigation();
+  const route = useRoute();
+  const { id } = route.params as RouteParams;
+
+  async function fetchProductById() {
+    setLoading(true);
+    const response = await getProductByID(id);
+    setProduct(response);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchProductById();
+  }, [])
   
   return (
     <Container>
       <Header
         showBackButton={true}
+        showCartIcon={true}
         title={'Detalhe do produto'}
       />
 
-      <ViewImageCenter>
-        <Image source={{uri: mockProduct.image}} />
-      </ViewImageCenter>
+      {loading || !product ?
+        <Loading />
+      :
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <ViewImageCenter>
+            <Image source={{uri: product.image}} resizeMode='contain'/>
+          </ViewImageCenter>
 
-      <HighLight
-        title={mockProduct?.title}
-        description={mockProduct?.description}
-        short={false}
-        detail={true}
-      />
+          <HighLight
+            title={product.title}
+            description={product.description}
+            short={false}
+            detail={true}
+          />
 
-      <ContainerValue>
-        <Value>
-          {'R$ ' + mockProduct.value}
-        </Value>
-        
-        <QuantityCount />
-      </ContainerValue>
-        
+          <ContainerValue>
+            <Value>
+              {'R$ ' + product.price}
+            </Value>
+            
+            <QuantityCount sizeIcon={48} value={quantity} setQuantity={setQuantity}/>
+          </ContainerValue>
 
-      <Button
-        title='Adicione ao carrinho'
-      />
+          <Button
+            title='Adicione ao carrinho'
+          />
+        </ScrollView>
+      }
     </Container>
   )
 }
