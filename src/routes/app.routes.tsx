727351d9aee968898674e77f802a52f3607a92c6
Jhonatan Holanda Maia <jhonatan_holanda@hotmail.com>
2023-08-29 14:27:59 -0300

    feat: :sparkles: Created routes of aplication

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,27 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { Products } from '../screens/Products';
+import { DetailProduct } from '../screens/DetailProduct';
+import { Cart } from '../screens/Cart';
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+export function AppRoutes() {
+  return(
+    <Navigator>
+      <Screen 
+        name="products"
+        component={Products}
+      />
+
+      <Screen 
+        name="detailProduct"
+        component={DetailProduct}
+      />
+
+      <Screen 
+        name="cart"
+        component={Cart}
+      />
+    </Navigator>
+  )
+}
