a10828229e60885320c0dfc66e1d6ed8ae7ab208
Jhonatan Holanda Maia <jhonatan_holanda@hotmail.com>
2023-08-29 14:42:03 -0300

    feat: :sparkles: Created services that will be requested in the api

---
+++ src/services/products.ts
@@ -0,0 +1,34 @@
+import { Alert } from "react-native";
+import { AppError } from "../utils/AppError";
+import { api } from "./api";
+import { ProductProps } from "../@types/productsDTO";
+
+export async function getAllProducts() {
+  try{
+    const response = await api.get('/products');
+    return response.data;
+  }catch(error) {
+    const isAppError = error instanceof AppError;
+
+    if(isAppError) {
+      Alert.alert(error.message)
+    }else{
+      Alert.alert("Não foi possível carregar os produtos, tente novamente mais tarde.")
+    }
+  }
+}
+
+export async function getProductByID(id:number) {
+  try{
+    const response = await api.get(`/products/${id}`);
+    return response.data;
+  }catch(error) {
+    const isAppError = error instanceof AppError;
+
+    if(isAppError) {
+      Alert.alert(error.message)
+    }else{
+      Alert.alert("Não foi possível carregar os dados do produto, tente novamente mais tarde.")
+    }
+  }
+}
