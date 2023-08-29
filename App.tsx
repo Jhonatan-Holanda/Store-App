8850d6c2e35e7645a056f299603fe1e5ef72704b
Jhonatan Holanda Maia <jhonatan_holanda@hotmail.com>
2023-08-29 16:01:29 -0300

    feat: :sparkles: Applying our routes in the application

--- App.tsx
+++ App.tsx
@@ -1,13 +1,12 @@
-import { StatusBar, ActivityIndicator } from 'react-native';
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
 import { RobotoMono_600SemiBold, RobotoMono_700Bold } from '@expo-google-fonts/roboto-mono';
 import { useFonts, EncodeSans_400Regular, EncodeSans_700Bold } from '@expo-google-fonts/encode-sans';
 
-import defaultTheme from './src/theme/default';
-import { Products } from './src/screens/Products';
-import { DetailProduct } from './src/screens/DetailProduct';
-import { Cart } from './src/screens/Cart';
 import React from 'react';
+import defaultTheme from './src/theme/default';
+import { Loading } from './src/components/Loading';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({RobotoMono_600SemiBold, RobotoMono_700Bold, EncodeSans_400Regular, EncodeSans_700Bold})
@@ -20,7 +19,7 @@ export default function App() {
         translucent
       />
 
-      {fontsLoaded ? <Products /> : <ActivityIndicator /> }
+      {fontsLoaded ? <Routes /> : <Loading /> }
     </ThemeProvider>
   );
 }
