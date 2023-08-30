c60a91d5d2f84a019e665000b7c57f9ed5ee3d88
Jhonatan Holanda Maia <jhonatan_holanda@hotmail.com>
2023-08-29 21:55:15 -0300

    feat: :sparkles: Fetching product data from the api and applying the add product to cart rule

--- src/screens/DetailProduct/index.tsx
+++ src/screens/DetailProduct/index.tsx
@@ -12,7 +12,9 @@ import { ProductProps } from '../../@types/productsDTO';
 
 import { Container, Image, ViewImageCenter, Value, ContainerValue } from "./styles";
 import { getProductByID } from '../../services/products';
-import { ScrollView, View } from "react-native";
+import { Alert, ScrollView, View } from "react-native";
+import { addProductInCart } from "../../storage/Cart/addProductCart";
+import { AppError } from "../../utils/AppError";
 
 type RouteParams = {
   id: string
@@ -21,7 +23,7 @@ type RouteParams = {
 export function DetailProduct(){
   const [product, setProduct] = useState<ProductProps>();
   const [loading, setLoading] = useState(false);
-  const [quantity, setQuantity] = useState(0);
+  const [quantity, setQuantity] = useState(1);
   const theme = useTheme();
 
   const navigation = useNavigation();
@@ -29,10 +31,50 @@ export function DetailProduct(){
   const { id } = route.params as RouteParams;
 
   async function fetchProductById() {
-    setLoading(true);
-    const response = await getProductByID(id);
-    setProduct(response);
-    setLoading(false);
+    try {
+      setLoading(true);
+      const response = await getProductByID(id);
+      setProduct(response);
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+
+      if(isAppError) {
+        Alert.alert(error.message)
+      }else{
+        Alert.alert("Não foi possível carregar osa dados do produto, tente novamente mais tarde.")
+      }
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  async function AddProductInCart(idProduct: string, quantity: number){
+    try {
+      if(quantity === 0){
+        return Alert.alert('Informe a quantidade que deseja adicionar ao carrinho');
+      }
+
+      await addProductInCart(idProduct, quantity);
+    }catch(error){
+      if(error instanceof AppError){
+        Alert.alert(error.message);
+      }else{
+        console.log(error);
+        Alert.alert('Não foi possível adicionar o produto no carrinho, tente novamente mais tarde.')
+      }
+    }
+  }
+
+  function handleAddProductInCart() {
+    AddProductInCart(id, quantity);
+    Alert.alert(
+      'Adicionar produto ao carrinho',
+      'Deseja continuar comprando?',
+      [
+        {text: "Sim", onPress: () => navigation.navigate('products')},
+        {text: "Ir para ao carrinho", onPress: () => navigation.navigate('cart')},
+      ]
+    )
   }
 
   useEffect(() => {
@@ -72,6 +114,7 @@ export function DetailProduct(){
 
           <Button
             title='Adicione ao carrinho'
+            onPress={handleAddProductInCart} 
           />
         </ScrollView>
       }
