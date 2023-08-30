45075f61ab1f945775127af317e241dd22733470
Jhonatan Holanda Maia <jhonatan_holanda@hotmail.com>
2023-08-30 16:06:45 -0300

    test: :clown_face: Created mocks that will be used in the test

---
+++ __tests__/mocks/api/mockProductsAPIResponse.ts
@@ -0,0 +1,21 @@
+
+export const mockProductsAPIResponse = [
+  {
+    id: '13',
+    title: 'Acer SB220Q bi 21.5 inches Full HD (1920 x 1080) IPS Ultra-Thin',
+    price: 599,
+    description: '21. 5 inches Full HD (1920 x 1080) widescreen IPS display And Radeon free Sync technology. No compatibility for VESA Mount Refresh Rng HDMI port Zero-frame design | ultra-thin | 4ms response time | IPS panel Aspect ratio - 16: 9. Color Supported - 16. 7 million colors. Brightness - 250 n5 degree to 15 degree. Horizontal viewing angle-178 degree. Vertical viewing angle-178 degree 75 hertz',
+    category: 'electronics',
+    image: 'https://fakestoreapi.com/img/81QpkIctqPL._AC_SX679_.jpg',
+    rating: { rate: 2.9, count: 250 }
+  },
+  {
+    id: '18',
+    title: "MBJ Women's Solid Short Sleeve Boat Neck V ",
+    price: 9.85,
+    description: '95% RAYON 5% SPANDEX, Made in USA or Imported, Do Not Bleach, Lightweight fabric with great stretch for comfort, Ribbed on sleeves and neckline / Double stitching on bottom hem',
+    category: 'electronics',
+    image: 'https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY879_.jpg',
+    rating: { rate: 2.9, count: 250 }
+  }
+]
---
+++ __tests__/mocks/component/mockProductsCardProducts.ts
@@ -0,0 +1,20 @@
+import { ProductProps } from "../../../src/@types/productsDTO";
+
+export const mockProductsCardProducts: ProductProps[] =  
+[
+  {
+
+    description: '95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.',
+    id: '20',
+    image: 'https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg',
+    price: 12.99,
+    title: 'DANVOUY Womens T Shirt Casual Cotton Short',
+  },
+  {
+    description: '95% RAYON 5% SPANDEX, Made in USA or Imported, Do Not Bleach, Lightweight fabric with great stretch for comfort, Ribbed on sleeves and neckline / Double stitching on bottom hem',
+    id: '18',
+    image: 'https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY879_.jpg',
+    price: 9.85,
+    title: "MBJ Women's Solid Short Sleeve Boat Neck V ",
+  },
+]
